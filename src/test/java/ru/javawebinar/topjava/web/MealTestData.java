cd318167947a9554645c41cc4bc8136c4661a035
serjy88 <mrserjy@gmail.com>
2020-10-16 21:28:46 +0300

    wip optional

---
+++ src/test/java/ru/javawebinar/topjava/web/MealTestData.java
@@ -0,0 +1,13 @@
+package ru.javawebinar.topjava.web;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+
+public class MealTestData {
+
+
+}
